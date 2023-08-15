9417d1a899d3be4c4ca39bd6df6edb4af759a3ae
sperilla <smperilla@gmail.com>
2023-08-14 21:04:31 -0400

    sign up functionality and form created

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,4 +7,19 @@ router.get("/login", (req, res) => {
   res.render("auth/login");
 });
 
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", async (req, res) => {
+  if (req.body.username && req.body.password) {
+    let plainTextPassword = req.body.password;
+    bcrypt.hash(plainTextPassword, 10, async (err, hashedPassword) => {
+      req.body.password = hashedPassword;
+      let newUser = await User.create(req.body);
+      res.send(newUser);
+    });
+  }
+});
+
 module.exports = router;
